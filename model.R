2d69f0bb81b4c0bbb5d0653894aa0bbb5be7f840
Lei Qian <lei.qian@duke.edu>
2015-10-31 11:49:22 -0400

    generating fake nyc data

--- model.R
+++ model.R
@@ -2,18 +2,49 @@ library(raster)
 library(e1071)
 library(dplyr)
 load("data.Rdata")
-load("NotNYC.Rdata")
 
 names(data) = c("Borough","long","lat")
+data1 <- sample_frac(data, 0.04)
 
-data1 <- sample_frac(data, 0.1)
+N <- 20000
+sample <- data.frame(x=runif(N,-74.3,-73.67),y=runif(N,40.5,40.91))
+sample$NYC <- rep("N",N)
+names(sample) <- c("long", "lat", "borough")
+##data below refers to the data we use for svm; colnames below are x and y, you might want to change to Latitude and Longitude when use
+#1 latitude = 111111 meters so 200 meters roughly = 200 * 1/111111 degree of latitude
+md200 <- 200 * 1/111111
+that <- sample_frac(data1, 0.4)
+notNYC <- function(this){
+  v <- that[that$long < this[1] + md200,]
+  if(dim(v)[1] == 0)
+    return ("N")
+  
+  v <- v[v$long > this[1] - md200,]
+  if(dim(v)[1] == 0)
+    return ("N")
+  
+  v <- v[v$lat < this[2] + md200,]
+  if(dim(v)[1] == 0)
+    return ("N")
+  
+  v <- v[v$lat > this[2] - md200,]
+  if(dim(v)[1] == 0)
+    return ("N")
+  
+  else
+    return("Y")
+}
+
+sample$NYC <- unlist(apply(sample[,1:2], 1, notNYC))
+
+Not_NYC <- filter(sample,NYC == "N")
+Not_NYC$borough <- NULL
+Not_NYC1 <- cbind(Not_NYC[,"NYC"], Not_NYC[,c("long", "lat")])
+colnames(Not_NYC1) <- c("Borough","long","lat")
+Not_NYC1$Borough <- rep("XNYC", nrow(Not_NYC1))
 
 
-sample$new <- sample$x
-sample$x <- NULL
-names(sample) <- c("Borough","long","lat")
-sample$Borough <- rep("XNYC", times = nrow(sample))
-data2 <- rbind(data1,sample)
+data2 <- rbind(data1,Not_NYC1)
 data2$long <- as.numeric(data2$long)
 data2$lat <- as.numeric(data2$lat)
 data2 <- na.omit(data2)
@@ -34,7 +65,8 @@ pred_locs = data.frame(xyFromCell(r,1:dims^2))
 colnames(pred_locs) <- c("long", "lat")
 pred = predict(l,pred_locs)
 r[] = pred
-plot(r)
+colors = c("red", "blue", "yellow", "green", "purple","white")
+plot(r, col = colors)
 points(data$long,data$lat,pch=16,cex=0.1)
 
 
