d72961835c98a0b9153f443ac384b6a1707fc59a
Lei Qian <lei.qian@duke.edu>
2015-11-06 21:46:59 -0500

    changed visual part

--- model.R
+++ model.R
@@ -99,3 +99,55 @@ poly@data$Name = short_to_long[levels(pred)[1:5]]
 
 source("write_geojson.R")
 write_geojson(poly,"boroughs.json")
+
+
+#Visual#
+source("geocode.R")
+nyc_interx <- nyc7.2 %>% 
+  select(Cross.Street.1,Cross.Street.2,Borough, Status)
+names(nyc_interx) <- c("Street1","Street2","borough", "Status")
+
+nyc_intery <- nyc7.2 %>% 
+  select(Intersection.Street.1,Intersection.Street.2,Borough, Status)
+
+names(nyc_intery) <- c("Street1","Street2","borough", "Status")
+
+nyc_interT <- rbind(nyc_interx,nyc_intery)
+nyc_interT <- unique(nyc_interT)
+#merge nyc_inter with inter_data
+
+#transform function brings different stylistic notations into a single one. 
+#If AVENUE then change to AVE, if STREET change to ST and so on. 
+#A str_c command was utilized to avoid the process of running through the data multiple times. 
+transform = function(v){
+  v = str_replace_all(str_c(v), 
+                      c("AVENUE"="AVE", "STREET"="ST", "DRIVE"="DR", 
+                        "BOULEVARD"="BLVD", "ROAD"="RD", "PLACE"="PL", 
+                        "EXPRESSWAY"="EXPY", "PARKWAY"="PKWY", 
+                        "FREEWAY"="FWY", "CRESCENT"="CRES", "COURT"="CT",
+                        "LANE"="LN", "EAST"="E", "WEST"="W", "SOUTH "="S",
+                        "NORTH"="N"))
+}
+
+nyc_interT$Street1 <- lapply(nyc_interT$Street1, transform)
+nyc_interT$Street2 <- lapply(nyc_interT$Street2, transform)
+inter_data$Street1 <- lapply(inter_data$Street1, transform)
+inter_data$Street2 <- lapply(inter_data$Street2, transform)
+
+stat1 <- merge(nyc_interT,inter_data)
+names(inter_data) <- c("Longitude","Latitude","Street2","Street1")
+stat2 <- merge(nyc_interT, inter_data)
+stat_inter <- rbind(stat1, stat2)
+stat_inter1 <- filter(stat_inter, Status == "Pending")
+stat_inter2 <- filter(stat_inter, Status == "Closed")
+stat_inter3 <- filter(stat_inter, Status == "Assigned")
+stat_inter4 <- filter(stat_inter, Status == "Open")
+stat_inter5 <- filter(stat_inter, Status == "Started")
+stat_inter6 <- filter(stat_inter, Status == "Unassigned")
+plot(stat_inter1$Longitude, stat_inter1$Latitude, pch = 16, cex = 0.01, col = "red")
+points(stat_inter2$Longitude, stat_inter2$Latitude, pch = 16, cex = 0.01, col = "blue")
+points(stat_inter3$Longitude, stat_inter3$Latitude, pch = 16, cex = 0.01, col = "orange")
+points(stat_inter4$Longitude, stat_inter4$Latitude, pch = 16, cex = 0.01, col = "green")
+points(stat_inter5$Longitude, stat_inter5$Latitude, pch = 16, cex = 0.01, col = "purple")
+points(stat_inter6$Longitude, stat_inter6$Latitude, pch = 16, cex = 0.01, col = "yellow")
+
